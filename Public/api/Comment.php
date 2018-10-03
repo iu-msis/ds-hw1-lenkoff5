1a0e200f6c65d810cbbaf0d2747a6896ff45407d
Lee <lenkoff@iu.edu>
2018-10-03 16:27:49 -0400

    Updated Hw1

--- Public/api/Comment.php
+++ Public/api/Comment.php
@@ -1,27 +1,13 @@
 <?php
-class Comment
-{
-  public $id;
-  public $comment;
 
-  public function __construct($data) {
-    $this->id = intval($data['id']);
-    $this->comment = $data['comment'];
-  }
-  public static function findAll() {
-    // 1. Connect to the database
-    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    // 2. Prepare the query
-    $sql = 'SELECT * FROM Class';
-    $statement = $db->prepare($sql);
-    // 3. Run the query
-    $success = $statement->execute();
-    // 4. Handle the results
-    $arr = [];
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-      $theComment =  new Comment($row);
-      array_push($arr, $theComment);
-    }
-    return $arr;
-  }
-}
+require '../../app/common.php';
+
+
+
+
+
+$comments = Comment::findAll();
+
+// 2. convert to Json
+header('Content-type: application/json');
+echo json_encode($comments);
