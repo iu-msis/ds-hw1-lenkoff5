7ef06b9f0afd427a45313a743cc9b8823fdd3c48
Lee <lenkoff@iu.edu>
2018-10-03 16:17:15 -0400

    HW updated

---
+++ app/data/Comment-stub.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS Class;
+
+CREATE TABLE Class (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  comment VARCHAR(31) NOT NULL
+);
+
+INSERT INTO Class (id, comment)
+VALUES (1, 'Go Cardinals');
+INSERT INTO Class (id, comment)
+VALUES (2, 'I love MSIS');
+INSERT INTO Class (id, comment)
+VALUES (3, 'I am a huge Billy Joel fan');
+INSERT INTO Class (id, comment)
+VALUES (4, 'Wine is good');
