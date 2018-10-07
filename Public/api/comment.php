9f913b439f010e6a81e9ba3a50668442ebaf0aa6
Lee <lenkoff@iu.edu>
2018-10-07 19:46:11 -0400

    Pretty JSON Format

--- Public/api/comment.php
+++ Public/api/comment.php
@@ -5,6 +5,9 @@ require '../../app/common.php';
 
 $comments = Comment::findAll();
 
+
+$json = json_encode($comments, JSON_PRETTY_PRINT);
+
 // 2. convert to Json
 header('Content-type: application/json');
-echo json_encode($comments);
+echo $json;
