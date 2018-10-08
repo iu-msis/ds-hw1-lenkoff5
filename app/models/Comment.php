48cccb812742cc4aff0befe0ed0f5e9f28972abe
Lee <lenkoff@iu.edu>
2018-10-07 20:23:10 -0400

    Edited Comment so I can POST

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -5,9 +5,33 @@ class Comment
   public $comment;
 
   public function __construct($row) {
-    $this->id = intval($row['id']);
+    $this->id = isset($row['id']) ? intval($row['id']) : null;
     $this->comment = $row['comment'];
   }
+
+  public function create() {
+    $DB = NEW PDO(DB_SERVER, DB_USER, DB_PW);
+
+    $sql = 'INSERT INTO Class (comment)
+            VAlUES (?)';
+
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->comment
+    ]);
+
+      $this->id = $db->lastInsertId();
+  }
+
+
+
+
+
+
+
+
+
+
   public static function findAll() {
     // 1. Connect to the database
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
