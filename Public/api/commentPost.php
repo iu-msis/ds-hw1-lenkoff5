400fe206bce7394f86f7732368689054c929b0f3
Lee <lenkoff@iu.edu>
2018-10-07 20:02:48 -0400

    commments instead of comment

--- Public/api/commentPost.php
+++ Public/api/commentPost.php
@@ -1,5 +1,5 @@
 <?php
 var_dump($_POST);
-$comment = new Comment($_POST);
-$comment->create();
-echo json_encode($comment);
+$comments = new Comment($_POST);
+$comments->create();
+echo json_encode($comments);
