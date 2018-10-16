e67a0b4346d39cdfe804e9a3b6aea3aa6b255c58
Lee <lenkoff@iu.edu>
2018-10-15 22:07:53 -0400

    bug fix

--- Public/js/comments.app.js
+++ Public/js/comments.app.js
@@ -1,60 +1,13 @@
 var commentsApp = new Vue({
-  el: '#commentMain',
+  el: '#commentTable',
   data: {
-    comments: {
-      id: 0,
-      comment: '',
-
-    },
-    commentsList:[]
+    comments:[],
+    commentForm: { }
   },
+
   methods: {
-    handleCommentForm(e) {
 
-      // TODO: Check validity in a better way
-
-      const s = document.stringify(this.commentForm);
-
-      console.log(s);
-
-      // POST to remote server
-      fetch('api/comment.php', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: s // body data type must match "Content-Type" header
-      })
-      .then( response => response.json() )
-      .then( json => {this.comment.push(json)})
-      .catch( err => {
-        console.error('WORK POST ERROR:');
-        console.error(err);
-      })
-
-      // Reset workForm
-      this.commentForm = this.getEmptyCommentForm();
-    },
-
-
-    getEmptyCommentForm() {
-      return {
-        id: this.comment.id,
-        comment: moment().format('HH:mm'),
-      }
-    },
-
-  },
-  created () {
-    // Do data fetch
-    const url = new URL(window.location.href);
-
-
-
-    // Populate workForm with default values
-
-    // TODO: Fetch task-specific data
-    // fetch('api/task?id=4')
+  fetchComments() {
     fetch('api/comment.php')
     .then( response => response.json() )
     .then( json => {commentsApp.commentList = json} )
@@ -62,7 +15,12 @@ var commentsApp = new Vue({
       console.error('WORK FETCH ERROR:');
       console.error(err);
     })
-
   }
+},
+
+created () {
+  // Do data fetch
+  this.fetchComments();
 }
-)
+
+})
