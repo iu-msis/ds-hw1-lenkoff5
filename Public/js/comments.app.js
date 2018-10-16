6c5de74bb6518fcc56921b2b10a4077d131f9db4
Lee <lenkoff@iu.edu>
2018-10-15 22:12:50 -0400

    bug fix

--- Public/js/comments.app.js
+++ Public/js/comments.app.js
@@ -10,7 +10,7 @@ var commentsApp = new Vue({
   fetchComments() {
     fetch('api/comment.php')
     .then( response => response.json() )
-    .then( json => {commentsApp.commentList = json} )
+    .then( json => {commentsApp.comments = json} )
     .catch( err => {
       console.error('WORK FETCH ERROR:');
       console.error(err);
@@ -24,3 +24,39 @@ created () {
 }
 
 })
+
+handleCommentForm(e) {
+
+  // TODO: Check validity in a better way
+
+  const s = JSON.stringify(this.commentForm);
+
+  console.log(s);
+
+  // POST to remote server
+  fetch('api/comment.php', {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+        "Content-Type": "application/json; charset=utf-8"
+    },
+    body: s // body data type must match "Content-Type" header
+  })
+  .then( response => response.json() )
+  .then( json => {this.comment.push(json)})
+  .catch( err => {
+    console.error('WORK POST ERROR:');
+    console.error(err);
+  })
+
+  // Reset workForm
+  this.commentForm = this.getEmptyCommentForm();
+}
+
+
+getEmptyCommentForm() {
+  return {
+    id: this.comment.id,
+    comment: moment().format('HH:mm'),
+  }
+}
+},
