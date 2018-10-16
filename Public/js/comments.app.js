85443c402e7215104215a1163d17ec44c77515de
Lee <lenkoff@iu.edu>
2018-10-15 22:18:45 -0400

    POST

--- Public/js/comments.app.js
+++ Public/js/comments.app.js
@@ -15,8 +15,44 @@ var commentsApp = new Vue({
       console.error('WORK FETCH ERROR:');
       console.error(err);
     })
+  },
+  handleCommentForm(e) {
+
+    // TODO: Check validity in a better way
+
+    const s = JSON.stringify(this.commentForm);
+
+    console.log(s);
+
+    // POST to remote server
+    fetch('api/comment.php', {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+          "Content-Type": "application/json; charset=utf-8"
+      },
+      body: s // body data type must match "Content-Type" header
+    })
+    .then( response => response.json() )
+    .then( json => {this.comment.push(json)})
+    .catch( err => {
+      console.error('COMMENT POST ERROR:');
+      console.error(err);
+    })
+
+    // Reset workForm
+    this.commentForm = this.getEmptyCommentForm();
+  },
+
+
+  getEmptyCommentForm() {
+    return {
+      id: this.comment.id,
+      comment: moment().format('HH:mm'),
+    }
   }
 },
+
+
 
 created () {
   // Do data fetch
